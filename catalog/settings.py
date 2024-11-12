11eb5bf8656b6717919d43b7cdd9e43544c8c11c
Mert Demircan <125318854+MertDmrcn@users.noreply.github.com>
2024-11-12 22:16:51 +0300

    2

--- catalog/settings.py
+++ catalog/settings.py
@@ -77,6 +77,7 @@ DATABASES = {
         "PASSWORD": "demircan3535",
         "HOST": "localhost",
         "PORT": "3306",
+        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
 
